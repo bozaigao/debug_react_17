2c57bdea1168a52702d175cd14a695f6a2f80a45
波仔糕 <1054539528@qq.com>
2020-12-27 10:38:28 +0800

    chore: fix

---
+++ src/myreact.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: 波仔糕
+ * @LastModifiedBy: 波仔糕
+ */
+import React from "./kreact/";
+import ReactDOM, {useState} from "./kreact/react-dom";
+import Component from "./kreact/Component";
+export{React,useState, Component,ReactDOM}
---
+++ src/react.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 波仔糕
+ * @LastModifiedBy: 波仔糕
+ */
+import * as React from "./react/packages/react";
+import { StatelessFunctionalComponent } from "./react/packages/react";
+import { useState, Component } from "./react/packages/react";
+import * as ReactDOM from "./react/packages/react-dom";
+
+export { useState, Component, ReactDOM, React }
